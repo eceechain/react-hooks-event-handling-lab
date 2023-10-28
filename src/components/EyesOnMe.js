41681634a590c0b5055656852100c5c9064ff9e1
ecee <you@example.com>
2023-10-28 15:54:49 +0300

    Done with event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+//Function to handle focus
+    function handleFocus(){
+        console.log('Good!')
+    }
+//Function to handle blur
+function handleBlur(){
+    console.log('Hey! Eyes on me!')
+}
+
+  return (
+    <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
